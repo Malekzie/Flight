5baa3d5520c87d6b2e2d0e930745cd779ba4fb76
Robbie <133696122+Malekzie@users.noreply.github.com>
2024-01-07 15:21:13 -0700

    Transferred header to root layout

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,28 @@
+<script>
+  import Icon from '@iconify/svelte';
+</script>
+
+<section class="flex justify-between">
+	<!-- Logo -->
+	<div class=" ml-10">
+		<a href="/"><img src="logo.png" alt="Flight Logo" class="w-auto h-32" /></a>
+	</div>
+	<!-- Searchbar -->
+	<div class="">
+		<input type="text" placeholder="Search" class="mt-10 w-[20rem] mx-5 rounded-lg text-black" />
+	</div>
+	<!-- Buttons -->
+	<div class=" flex justify-center items-center gap-3">
+		<a href="/">A</a>
+		<a href="/">A</a>
+		<a href="/">A</a>
+		<a href="/">A</a>
+		<a href="/">A</a>
+		<a href="/">A</a>
+	</div>
+	<!-- Login -->
+	<div class=" mr-10 flex justify-center items-center gap-2">
+		<a href="/member/login"><Icon icon="gg:profile" class="text-3xl"/></a>
+		<button><Icon icon="mdi:cart-outline" class="text-3xl"/></button>
+	</div>
+</section>
