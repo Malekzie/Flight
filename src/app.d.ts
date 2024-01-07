f9b0581479bd02b9f7c5d8d26aaa8e71ed2d176c
Robbie <133696122+Malekzie@users.noreply.github.com>
2024-01-07 01:36:51 -0700

    updated namespaces

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,9 +1,31 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 // and what to do when importing types
-declare namespace App {
-	// interface Locals {}
-	// interface PageData {}
-	// interface Error {}
-	// interface Platform {}
+
+import type { PrismaClient } from "@prisma/client";
+
+declare global {
+
+	declare namespace App {
+		interface Locals {
+			auth: import("lucia").AuthRequest;
+			validate: import("@lucia-auth/sveltekit").Validate
+			validateUser: import("@lucia-auth/sveltekit").ValidateUser
+			setSession: import("@lucia-auth/sveltekit").SetSession
+		}
+		// interface PageData {}
+		// interface Error {}
+		// interface Platform {}
+	}
+	const __prisma: PrismaClient
+
+	namespace Lucia {
+		type Auth = import("$lib/server/lucia").Auth;
+		type DatabaseUserAttributes = {
+			username: string;
+		};
+		type DatabaseSessionAttributes = {};
+	}
 }
+
+export {}; 
