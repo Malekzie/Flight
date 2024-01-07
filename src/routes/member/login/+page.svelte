a45c79269af8e137df7234a7e6bc93a0dc8dbf09
Robbie <133696122+Malekzie@users.noreply.github.com>
2024-01-07 02:39:20 -0700

    Centered form

--- src/routes/member/login/+page.svelte
+++ src/routes/member/login/+page.svelte
@@ -6,16 +6,18 @@
 	export let form: ActionData;
 
 </script>
-
-<h1>Sign in</h1>
-<form method="post" use:enhance>
-	<label for="username">Username</label>
-	<input name="username" id="username" class="text-black"/><br />
-	<label for="password">Password</label>
-	<input type="password" name="password" id="password" class="text-black"/><br />
-	<button type="submit">Submit</button>
-</form>
-{#if form?.message}
-	<p>{form.message}</p>
-{/if}
-<a href="/member/register">Create an account</a>
+<div class="flex flex-col justify-center items-center h-screen align-middle">
+	
+	<h1>Login to your account</h1>
+	<form method="post" use:enhance>
+		<label for="username">Username</label>
+		<input name="username" id="username" class="text-black"/><br />
+		<label for="password">Password</label>
+		<input type="password" name="password" id="password" class="text-black"/><br />
+		<button type="submit" class="btn variant-filled-surface rounded-lg mt-2 w-full ">Submit</button>
+	</form>
+	{#if form?.message}
+		<p>{form.message}</p>
+	{/if}
+	<a href="/member/register">Create an account</a>
+</div>
--- src/routes/member/register/+page.svelte
+++ src/routes/member/register/+page.svelte
@@ -5,16 +5,18 @@
 
 	export let form: ActionData;
 </script>
-
-<h1>Sign up</h1>
-<form method="post" use:enhance>
-	<label for="username">Username</label>
-	<input name="username" id="username" class="text-black"/><br />
-	<label for="password">Password</label>
-	<input type="password" name="password" id="password" class="text-black"/><br />
-	<button type="submit">Submit</button>
-</form>
-{#if form?.message}
-	<p>{form.message}</p>
-{/if}
-<a href="/member/login">Sign in</a>
+<div class="flex flex-col justify-center align-middle items-center h-screen">
+	
+	<h1>Register to flight!</h1>
+	<form method="post" use:enhance>
+		<label for="username">Username</label>
+		<input name="username" id="username" class="text-black"/><br />
+		<label for="password">Password</label>
+		<input type="password" name="password" id="password" class="text-black"/><br />
+		<button type="submit" class="btn variant-filled-surface rounded-lg mt-2 w-full ">Submit</button>
+	</form>
+	{#if form?.message}
+		<p>{form.message}</p>
+	{/if}
+	<a href="/member/login">Sign in</a>
+</div>
